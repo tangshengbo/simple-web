b0e49b4d0ddb261e4b1aad014f85559c402d8228
tangshengbo <867850520tangbo@sian.com>
2018-01-07 22:22:27 +0800

    add smart framework code

--- src/main/java/com/tangshengbo/controller/CustomerController.java
+++ src/main/java/com/tangshengbo/controller/CustomerController.java
@@ -4,8 +4,10 @@ import com.tangshengbo.annotation.Controller;
 import com.tangshengbo.annotation.Inject;
 import com.tangshengbo.annotation.RequestMapping;
 import com.tangshengbo.bean.Data;
+import com.tangshengbo.bean.FileParam;
 import com.tangshengbo.bean.Param;
 import com.tangshengbo.bean.View;
+import com.tangshengbo.helper.UploadHelper;
 import com.tangshengbo.model.Customer;
 import com.tangshengbo.service.CustomerService;
 import org.slf4j.Logger;
@@ -49,8 +51,8 @@ public class CustomerController {
         return new View("error.html");
     }
 
-    @RequestMapping("get:/create_customer")
-    public Data createCustomer() {
+    @RequestMapping("get:/create_customers")
+    public Data createCustomers() {
         logger.info("执行方法 参数 {}");
         Map<String, Object> fieldMap = new HashMap<>();
         fieldMap.put("name", "");
@@ -67,5 +69,18 @@ public class CustomerController {
         Data data = new Data();
         data.setModel("成功");
         return data;
+    }
+
+    @RequestMapping("get:/create_customer")
+    public View createCustomer() {
+        return new View("customer_create.jsp");
+    }
+    @RequestMapping("post:/create_customer")
+    public Data createCustomer(Param param) {
+        Map<String, Object> fieldMap = param.getFieldMap();
+        FileParam fileParam = param.getFile("photo");
+        UploadHelper.uploadFile("E:/", fileParam);
+        logger.info("执行方法 createCustomer fieldMap{} fileParam{}", fieldMap, fileParam);
+        return new Data("OK");
     }
 }
